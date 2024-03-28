afb6654c4af79bf94b28ca8b312dca57d0174925
Adnan ibrahim <adnanibrahi96@gmail.com>
2024-03-28 16:35:32 +0530

    going with python

---
+++ strings.py
@@ -0,0 +1,4 @@
+if(5>6):
+    print("5 is greater than 3")
+else:
+    print("5 is not greater than 3")
---
+++ test.py
@@ -0,0 +1,4 @@
+def python():
+    return print("Hello Welcome to Python Version 3.12")
+
+python()
