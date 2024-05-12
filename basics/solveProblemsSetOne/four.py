ac32868fa1060cc641eb7af3e81c14d768251a83
Adnan ibrahim <adnanibrahi96@gmail.com>
2024-05-12 23:52:00 +0530

    praticing..

---
+++ basics/solveProblemsSetOne/four.py
@@ -0,0 +1,12 @@
+fruit = "Banana"
+
+color = "yellow"
+if(fruit == "Banana"):
+    if(color == "green"):
+        print("unripe")
+    elif(color == "yellow"):
+        print("ripe")
+    elif(color == "brown"):
+        print("rotten")
+else:
+    print("not a banana")
