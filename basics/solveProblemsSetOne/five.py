b56cf36338aeb8ab61fd34f3ec9e6def605e08f4
Adnan ibrahim <adnanibrahi96@gmail.com>
2024-05-12 23:56:36 +0530

    problem praticing

---
+++ basics/solveProblemsSetOne/five.py
@@ -0,0 +1,10 @@
+weather = "sunny"
+
+if(weather == "sunny"):
+    print("Go for a walk")
+elif(weather == "rainy"):
+    print("Take an umbrella")
+elif(weather == "snowy"):
+    print("Wear a coat")
+else:
+    print("Do whatever you want to do...")
